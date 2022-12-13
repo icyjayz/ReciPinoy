f2921dfe588357bb77b8b27fbd5069065a07aaa3
eagmn <eagmn@gmail.com>
2022-12-13 08:30:23 +0800

    pwa config

--- public/js/main.js
+++ public/js/main.js
@@ -7,3 +7,17 @@
 //         console.log('registration failed...', err);
 //     })
 // }
+
+const registerServiceWorker = async () => {
+    if ("serviceWorker" in navigator) {
+        try {
+            let registration = await navigator.serviceWorker.register('/sw.js')
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+          } catch(err) {
+            console.log('ServiceWorker registration failed: ', err);
+          }
+    }
+  };
+  
+  
+registerServiceWorker();
